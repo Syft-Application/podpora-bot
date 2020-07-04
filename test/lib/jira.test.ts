5ef712e8b43acfbfef2aa065e642b37270cdafa5
Marek L <nospam.keram@gmail.com>
2020-07-04 16:14:07 +0100

    Improve code architecture for future improvements
    Hide creation of support request and all details into slack_team.createSupportRequest

--- test/lib/jira.test.ts
+++ test/lib/jira.test.ts
@@ -3,14 +3,9 @@ import { Logger } from 'winston';
 import { fixture } from '../helpers';
 import logger from '../../src/util/logger';
 import {
-    IssueWithUrl,
-    Jira
+    Jira, Issue
 } from '../../src/lib/jira';
-import {
-    SubmissionType,
-    SupportRequest,
-    BugSubmission
-} from '../../src/lib/slack_team';
+import issueParams from '../../src/lib/jira_create_issue_params';
 
 const createIssueResponse = fixture('jira/issues.createIssue.response');
 
@@ -21,33 +16,16 @@ const bug_report = {
         id: 'UHAV00MD0',
         name: 'sherlock_holmes'
     },
-    type: SubmissionType.BUG,
     submission: {
         title: 'bug report title',
         description: 'bug report description',
         expected: 'we like this to happen',
         currently: 'this is what happens now',
-        type: SubmissionType.BUG
-    } as BugSubmission,
+    },
     url: 'http://example.com/bug',
-    channel: 'CHS7JQ7PY'
-} as SupportRequest;
-const data_request = {
-    id: '1592066203.000200',
-    team_id: 'THS7JQ2RL',
-    user: {
-        id: 'UHAV00MD1',
-        name: 'john_watson'
-    },
-    type: SubmissionType.DATA,
-    submission: {
-        title: 'data request title',
-        description: 'data request description',
-        type: SubmissionType.DATA
-    },
-    url: 'http://example.com/data',
-    channel: 'CHS7JQ7PY'
-} as SupportRequest;
+    channel: 'CHS7JQ7PY',
+    state: 'bug'
+};
 
 beforeAll(() => {
     return nock.disableNetConnect();
@@ -69,16 +47,16 @@ describe('Jira', () => {
     };
 
     const jira = new Jira(mock_config);
+    const issue = {
+        ...createIssueResponse,
+    } as Issue;
 
-    describe('#createIssue()', () => {
-        const issueWithUrl = {
-            ...createIssueResponse,
-            url: `${mock_config.host}/browse/${createIssueResponse.key}`,
-            slack_channel_id: bug_report.channel,
-            slack_thread_id: bug_report.id
-        } as IssueWithUrl;
-
-        it('returns a Promise to create issue and link it to the slack thread', (done) => {
+    describe('#createIssue(params)', () => {
+        const submission = bug_report.submission;
+        const user = bug_report.user;
+        const request_type = 'bug';
+        const params = issueParams(submission, user, request_type);
+        it('returns a Promise that resolves to issue object', (done) => {
             let api_call_body: string;
             expect.assertions(7);
             nock(mock_config.host)
@@ -87,19 +65,15 @@ describe('Jira', () => {
                     return body;
                 }).reply(200, createIssueResponse);
 
-            nock(mock_config.host)
-                .post(`/rest/api/2/issue/${createIssueResponse.key}/remotelink`)
-                .reply(200);
-
-            jira.createIssue(bug_report)
+            jira.createIssue(params)
                 .then((res) => {
-                    const submission = bug_report.submission as BugSubmission;
-                    expect(res).toEqual(issueWithUrl);
+                    const submission = bug_report.submission;
+                    expect(res).toEqual(issue);
                     expect(api_call_body).toContain(submission.title);
                     expect(api_call_body).toContain(submission.description);
                     expect(api_call_body).toContain(submission.expected);
                     expect(api_call_body).toContain(submission.currently);
-                    expect(api_call_body).toContain(bug_report.user.name);
+                    expect(api_call_body).toContain(user.name);
                     expect(api_call_body).toContain('Bug');
                     done();
                 }).catch(done);
@@ -115,7 +89,7 @@ describe('Jira', () => {
                     .post('/rest/api/2/issue')
                     .reply(500, createIssueResponse);
 
-                jira.createIssue(bug_report)
+                jira.createIssue(params)
                     .catch((res) => {
                         expect(loggerSpy).toHaveBeenCalled();
                         const logger_call = loggerSpy.mock.calls[0].toString();
@@ -128,64 +102,64 @@ describe('Jira', () => {
             });
         });
 
-        describe('link slack message to an issue fails', () => {
-            it('it catch and log the failure but resolves successfuly', (done) => {
-                const loggerSpy = jest.spyOn(logger, 'error')
-                    .mockReturnValue({} as Logger);
-                expect.assertions(3);
-                nock(mock_config.host)
-                    .post('/rest/api/2/issue')
-                    .reply(200, createIssueResponse);
+        // describe('data request', () => {
+        //     const request_type = 'data';
 
-                nock(mock_config.host)
-                    .post(
-                        `/rest/api/2/issue/${createIssueResponse.key}/remotelink`
-                    ).reply(503, {});
+        //     it('changes issuetype to data', (done) => {
+        //         let api_call_body: string;
+        //         expect.assertions(5);
+        //         nock(mock_config.host)
+        //             .post('/rest/api/2/issue', (body) => {
+        //                 api_call_body = JSON.stringify(body);
+        //                 return body;
+        //             }).reply(200, createIssueResponse);
 
-                jira.createIssue(bug_report)
-                    .then((res) => {
-                        expect(loggerSpy).toHaveBeenCalled();
-                        const logger_call = loggerSpy.mock.calls[0].toString();
-                        expect(logger_call).toEqual(
-                            expect.stringContaining('linkRequestToIssue')
-                        );
-                        expect(res).toEqual(issueWithUrl);
-                        done();
-                    });
-            });
+        //         nock(mock_config.host)
+        //             .post(`/rest/api/2/issue/${createIssueResponse.key}/remotelink`)
+        //             .reply(200);
+
+        //         jira.createIssue(data_request, request_type)
+        //             .then((res) => {
+        //                 const submission = data_request.submission;
+        //                 expect(res).toEqual(issue);
+        //                 expect(api_call_body).toContain(submission.title);
+        //                 expect(api_call_body).toContain(submission.description);
+        //                 expect(api_call_body).toContain(data_request.user.name);
+        //                 expect(api_call_body).toContain('Task');
+        //                 done();
+        //             }).catch(done);
+        //     });
+        // });
+    });
+
+    describe('#addSlackThreadUrlToIssue(url, issue)', () => {
+        const url = 'some random url';
+
+        it('returns promise', (done) => {
+            nock(mock_config.host)
+                .post(
+                    `/rest/api/2/issue/${issue.key}/remotelink`
+                ).reply(200, { foo: 123 });
+
+            jira.addSlackThreadUrlToIssue(url, issue)
+                .then((res) => {
+                    expect(res).toEqual({ foo: 123 });
+                    done();
+                });
         });
 
-        describe('data request', () => {
-            const issueWithUrl = {
-                ...createIssueResponse,
-                url: `${mock_config.host}/browse/${createIssueResponse.key}`,
-                slack_channel_id: data_request.channel,
-                slack_thread_id: data_request.id
-            } as IssueWithUrl;
-
-            it('changes issuetype to data', (done) => {
-                let api_call_body: string;
-                expect.assertions(5);
+        describe('api failure', () => {
+            it('returns promise that rejects', (done) => {
                 nock(mock_config.host)
-                    .post('/rest/api/2/issue', (body) => {
-                        api_call_body = JSON.stringify(body);
-                        return body;
-                    }).reply(200, createIssueResponse);
+                    .post(
+                        `/rest/api/2/issue/${issue.key}/remotelink`
+                    ).reply(500, { foo: 123 });
 
-                nock(mock_config.host)
-                    .post(`/rest/api/2/issue/${createIssueResponse.key}/remotelink`)
-                    .reply(200);
-
-                jira.createIssue(data_request)
-                    .then((res) => {
-                        const submission = data_request.submission;
-                        expect(res).toEqual(issueWithUrl);
-                        expect(api_call_body).toContain(submission.title);
-                        expect(api_call_body).toContain(submission.description);
-                        expect(api_call_body).toContain(data_request.user.name);
-                        expect(api_call_body).toContain('Task');
+                jira.addSlackThreadUrlToIssue(url, issue)
+                    .catch((res) => {
+                        expect(res).toEqual({ ok: false });
                         done();
-                    }).catch(done);
+                    });
             });
         });
     });
